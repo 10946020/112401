bc964b6c4e175069f682f6eb410cdab6fb32594d
10946020 <10946020@ntub.edu.tw>
2023-10-13 17:54:11 +0800

    更換指紋辨識判斷的function, 並能顯示辨識成功或失敗

--- Arduino/Combined/Combined.ino
+++ Arduino/Combined/Combined.ino
@@ -44,7 +44,8 @@ void setup(){ //開始時執行一次, 主要為偵測
     while (1) { delay(1); }
   }
   finger.getTemplateCount();
-  Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates"); //顯示目前儲存了幾個指紋樣本
+  //顯示目前儲存了幾個指紋樣本
+  Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates"); 
 
   //---Keypad---
   pwDigit = 0; //
@@ -64,120 +65,161 @@ void setup(){ //開始時執行一次, 主要為偵測
 
 void loop(){ //重複執行的動作
   char key = keypad.getKey(); // Read the key
-  finger_status = getFingerprintIDez();
-
-  //按下按鈕時
-  if(key){
-    if(pwDigit == 0){ //原本顯示著Waiting, 先清空
-      lcd.clear();
-    }
-
-    //基本安全設定
-    if(pwDigit >= 1){ //輸入至少1碼後再有新的輸入的時候
-      for(int i = 0; i < pwDigit; i++){
-        //從最左上角開始把前面輸入的密碼都換成*, 避免旁邊的人看到
-        lcd.setCursor(i, 0);
-        lcd.print("*");
-      }
-    }
-
-    //從最左上方開始往右顯示按下的按鈕
-    lcd.setCursor(pwDigit, 0);
-    Serial.print("*"); //IDE視窗
-    userEnter += key;
-    pwDigit ++;
-    lcd.print(key); //LCD
-
-    if(pwDigit == password.length()){ //當輸入的密碼位數跟預設密碼的位數一樣時
-        //不管正確與否都要清空,並準備顯示其他訊息
-        lcd.clear();
-        lcd.setCursor(0, 0); //從第一行最左邊開始顯示
-
-      if(userEnter == password){ //正確
-        Serial.println("\nWelcome!");
-        //LCD顯示以下訊息
-        lcd.print("Validated");
-        lcd.setCursor(0, 1); //從第二行最左邊開始顯示
-        lcd.print("Welcome!");
-      }
-      else{
-        Serial.println("\nIncorrect Password.");
-        lcd.print("Try again!");
-      }
-
-      //清空位元數跟輸入的密碼字串, 等待3秒
+  finger_status = getFingerprintID();
+  //狀態2表示沒偵測到指紋, 寫個if防止迴圈一直更新
+  if(finger_status != 2){  //偵測到指紋辨識後等getFingerprintID()動作完, 重置狀態
       pwDigit = 0;
       userEnter = "";
       delay(3000);
-
-      //LCD顯示待機訊息
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Waiting...");
       Serial.print("Please enter the password or fingerprint : ");
-    }
   }
+  else{
+    //按下按鈕時
+    if(key){
+      if(pwDigit == 0){ //原本顯示著Waiting, 先清空
+        lcd.clear();
+      }
 
-  //或是不用按鈕, 指紋感測到時
-  else if(finger_status!=-1 and finger_status!=-2){
-    Serial.println("Match");
+      //基本安全設定
+      if(pwDigit >= 1){ //輸入至少1碼後再有新的輸入的時候
+        for(int i = 0; i < pwDigit; i++){
+          //從最左上角開始把前面輸入的密碼都換成*, 避免旁邊的人看到
+          lcd.setCursor(i, 0);
+          lcd.print("*");
+        }
+      }
 
-    Serial.println("Welcome back, User" + String(finger.fingerID));
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Hello, User" + String(finger.fingerID)); //指紋可以特定身分, 可以直接顯示名字
-    //可以設定感測到哪個指紋時要顯示出什麼訊息
-    /*
-    switch(finger.fingerID){
-        case 1:
-          Serial.println("Welcome back, User" + finger.fingerID.toString() + "!");
+      //從最左上方開始往右顯示按下的按鈕
+      lcd.setCursor(pwDigit, 0);
+      Serial.print("*"); //IDE視窗
+      userEnter += key;
+      pwDigit ++;
+      lcd.print(key); //LCD
+
+      if(userEnter == "#114514"){  //進入註冊模式
+
+      }
+      else if(userEnter == "#228922"){  //進入刪除身分模式
+
+      }
+      else if(pwDigit == password.length()){ //當輸入的密碼位數跟預設密碼的位數一樣時
+          //不管正確與否都要清空,並準備顯示其他訊息
           lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("Hello, User" + finger.fingerID.toString() + "!"); //指紋可以特定身分, 可以直接顯示名字
-          break;
-        case 2:
-          Serial.println("Welcome back, User02!");
-          lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("Hello, User02!");
-          break;
-        case 3:
-          Serial.println("Welcome back, User03!");
-          lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("Hello, User03!");
-          break;          
+          lcd.setCursor(0, 0); //從第一行最左邊開始顯示
+
+        if(userEnter == password){ //正確
+          Serial.println("\nWelcome!");
+          //LCD顯示以下訊息
+          lcd.print("Validated");
+          lcd.setCursor(0, 1); //從第二行最左邊開始顯示
+          lcd.print("Welcome!");
+        }
+        else{
+          Serial.println("\nIncorrect Password.");
+          lcd.print("Wrong password!");
+        }
+
+        //清空位元數跟輸入的密碼字串, 等待3秒
+        pwDigit = 0;
+        userEnter = "";
+        delay(3000);
+
+        //LCD顯示待機訊息
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Waiting...");
+        Serial.print("Please enter the password or fingerprint : ");
+      }
     }
-*/
-    pwDigit = 0;
-    userEnter = "";
-    delay(3000);
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Waiting...");
-    Serial.print("Please enter the password or fingerprint : ");
   }
 }
 
 //---------------------------------------------------------------------------------------------------------------
 
-// returns -1 if failed, otherwise returns ID #
-int getFingerprintIDez() {
+uint8_t getFingerprintID() {
   uint8_t p = finger.getImage();
-  //if (p!=2){Serial.println(p);}
-  if (p != FINGERPRINT_OK)  return -1;
-  
-  p = finger.image2Tz();
-  //if (p!=2){Serial.println(p);}
-  if (p != FINGERPRINT_OK)  return -1;
+  switch (p) {
+    case FINGERPRINT_OK:
+      //Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      //Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
 
-  p = finger.fingerFastSearch();
-  if (p != FINGERPRINT_OK)  return -2;
-  
+  // OK success!
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      //Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK converted!
+
+
+  //尋找指紋資訊並判斷是哪個身分, 或是掃描到的指紋存不存在
+  p = finger.fingerSearch();
+  if (p == FINGERPRINT_OK) {  //找到人
+    Serial.println("\nFound a print match!");
+    Serial.println("Welcome back, User" + String(finger.fingerID));
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Hello, User" + String(finger.fingerID)); //指紋可以特定身分, 可以直接顯示名字
+  }
+  else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  }
+  else if (p == FINGERPRINT_NOTFOUND) {  //沒找到
+    Serial.println("\nDid not find a match");
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Didn't found");
+    lcd.setCursor(0, 1);
+    lcd.print("fingerprint.");
+    return p;
+  }
+  else {
+    Serial.println("Unknown error");
+    return p;
+  }
+
   // found a match!
-  Serial.print("\nFound ID #"); Serial.print(finger.fingerID);
+  Serial.print("Found ID #"); Serial.print(finger.fingerID);
   Serial.print(" with confidence of "); Serial.println(finger.confidence);
-  return finger.fingerID; 
+
+  return finger.fingerID;
 }
 
 /*
