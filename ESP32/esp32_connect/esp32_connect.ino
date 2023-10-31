948ee6c9b4585598529a297f1a911700063bd786
10946020 <10946020@ntub.edu.tw>
2023-10-31 21:21:20 +0800

    ESP32 update: 可隨時偵測是否有連上Wifi, 並給出回應. 關閉傳遞UNO板訊息的功能
    傳遞訊息功能等Server建起來再開發

--- ESP32/esp32_connect/esp32_connect.ino
+++ ESP32/esp32_connect/esp32_connect.ino
@@ -2,23 +2,37 @@
 
 #define RXp2 16
 #define TXp2 17
-
 #define ledPin 32
-bool ledStatus = false;
+bool ledStatus = false;  //一開始先讓LED為暗
 
-const char ssid[]="_"; //自家WiFi網路名稱
-const char pwd[]="_"; //WiFi密碼
+const char ssid[] = "_"; //自家WiFi網路名稱
+const char pwd[] = "_"; //WiFi密碼
+
+void Show_Wifi_Status(){  //led腳位顯示當前狀態為true or false
+  delay(20);
+  digitalWrite(ledPin, ledStatus);
+}
+
+void Show_Wifi_Data(){  //顯示Wifi訊息用
+  Serial.print("IP位址:");
+  Serial.println(WiFi.localIP()); //讀取IP位址
+  Serial.print("WiFi RSSI:");
+  Serial.println(WiFi.RSSI()); //讀取WiFi強度
+}
 
 void setup() {
   Serial.begin(115200);
-  Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);  //第二個Serial, 顯示從16,17腳位接收到的訊息
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, ledStatus);
+
+  //第二個Serial, 顯示從16,17腳位接收到的訊息
+  //Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);
+  
+  pinMode(ledPin, OUTPUT);  //設定為輸出腳位
+  Show_Wifi_Status();
 
   WiFi.mode(WIFI_STA); //設置WiFi模式
-  WiFi.begin(ssid,pwd); 
+  WiFi.begin(ssid,pwd); //Wifi, 啟動
 
-  Serial.println("WiFi connecting");
+  Serial.print("WiFi connecting");
 
   //當WiFi連線時會回傳WL_CONNECTED，因此跳出迴圈時代表已成功連線
   while(WiFi.status()!=WL_CONNECTED){
@@ -26,15 +40,31 @@ void setup() {
     delay(500);   
   }
   Serial.println("\nConnected!");
-  Serial.print("IP位址:");
-  Serial.println(WiFi.localIP()); //讀取IP位址
-  Serial.print("WiFi RSSI:");
-  Serial.println(WiFi.RSSI()); //讀取WiFi強度
+  Show_Wifi_Data();
 
   ledStatus = true;
-  digitalWrite(ledPin, ledStatus);
+  Show_Wifi_Status();
 }
+
 void loop() {
-  Serial.println(Serial2.readString());
-  delay(50);
+  //Serial.println(Serial2.readString());
+  //delay(50);  //每50ms檢查一次從RX跟TX傳來的訊息
+  if(WiFi.status()!=WL_CONNECTED){
+    //如果Wifi斷掉連線就讓LED為暗, 然後進入while迴圈直到重新連上Wifi
+    ledStatus = false;
+    Show_Wifi_Status();
+    Serial.println("Wifi disconnected!!!");
+    Serial.print("Waiting for reconnection");
+
+    while(WiFi.status()!=WL_CONNECTED){
+      Serial.print(".");
+      delay(1000); 
+    }
+
+    //重新連上後顯示Wifi訊息, 以及讓提示用LED為亮
+    Serial.println("Wifi reconnected!");
+    Show_Wifi_Data();
+    ledStatus = true;
+    Show_Wifi_Status();
+  }
 }
