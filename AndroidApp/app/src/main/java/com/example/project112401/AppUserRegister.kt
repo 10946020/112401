3e14601937bd1e15ae84f6c5f9a0a76f663d31f7
10946020 <10946020@ntub.edu.tw>
2023-11-07 22:24:49 +0800

    App Update : 輸入的字串可傳到不同頁面; 修正了無法正常切換頁面,時間資料被清除等問題.
    接下來要建立app使用者的物件, 並試著進行把整個物件傳到別的頁面或存進database等操作

--- AndroidApp/app/src/main/java/com/example/project112401/AppUserRegister.kt
+++ AndroidApp/app/src/main/java/com/example/project112401/AppUserRegister.kt
@@ -5,29 +5,54 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
+private class AppUser(var name : String, var email : String, var password : String){
+    init {
+        this.name = name
+        this.email = email
+        this.password = password
+    }
+}
 class AppUserRegister : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
+
+    //宣告給使用者輸入物件的type
+    private lateinit var userName : EditText
+    private lateinit var userEmail : EditText
+    private lateinit var userPassword : EditText
+    private lateinit var checkPassword : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_user_register)
 
-        val userName = findViewById<TextView>(R.id.appUserRegister_Name)  //使用者輸入的名字
-        val userEmail = findViewById<TextView>(R.id.appUserRegister_Email)  //使用者輸入的Email
-        val userPassword = findViewById<TextView>(R.id.appUserRegister_Password)  //使用者輸入的密碼
-        val checkPassword = findViewById<TextView>(R.id.appUserRegister_CheckPW)  //重複確認使用者輸入的密碼
+        //依id來設定每個物件
+        userName = findViewById(R.id.appUserRegister_Name)  //使用者輸入的名字
+        userEmail = findViewById(R.id.appUserRegister_Email)  //使用者輸入的Email
+        userPassword = findViewById(R.id.appUserRegister_Password)  //使用者輸入的密碼
+        checkPassword = findViewById(R.id.appUserRegister_CheckPW)  //重複確認使用者輸入的密碼
 
+        val warningMessage = findViewById<TextView>(R.id.warning_sign)
         val registerBtn =  findViewById<Button>(R.id.button_toRegister)  //註冊按鈕
         val backBtn = findViewById<Button>(R.id.button_back)  //取消按鈕
 
+        //設定按下按鈕後會觸發的動作
         registerBtn.setOnClickListener {
-
+            val newUser = AppUser(userName.text.toString(),userEmail.text.toString(), userPassword.text.toString())
+            if(newUser.name == ""){
+                warningMessage.text = "Please Enter your name!"
+            }
+            else if(newUser.name != ""){
+                intent.putExtra("user_Name", newUser.name)
+                setResult(RESULT_OK, intent)
+                finish()
+            }
         }
 
         backBtn.setOnClickListener {
-            val intentToMainList = Intent(this, SecActivity::class.java)
-            startActivity(intentToMainList)
+            setResult(RESULT_CANCELED)
+            finish()
         }
     }
 }
