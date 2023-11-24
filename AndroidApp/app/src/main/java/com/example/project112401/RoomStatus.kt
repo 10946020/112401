1fe99dc2ca29f0c3ce812fece850dea58a8c4f22
10946020 <10946020@ntub.edu.tw>
2023-11-24 23:14:22 +0800

    App adjust : 整理按鈕選擇介面的code, 同時修正了部分code引起的bug

--- AndroidApp/app/src/main/java/com/example/project112401/RoomStatus.kt
+++ AndroidApp/app/src/main/java/com/example/project112401/RoomStatus.kt
@@ -2,11 +2,18 @@ package com.example.project112401
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RoomStatus : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room_status)
+
+        val backBtn = findViewById<Button>(R.id.roomBackBtn)
+        backBtn.setOnClickListener {
+            setResult(RESULT_CANCELED)
+            finish()
+        }
 
         //顯示房間資訊跟出入紀錄等, 紀錄用adapter弄
     }
--- AndroidApp/app/src/main/java/com/example/project112401/SecActivity.kt
+++ AndroidApp/app/src/main/java/com/example/project112401/SecActivity.kt
@@ -16,39 +16,37 @@ class SecActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sec)
 
-        val returnBtn = findViewById<Button>(R.id.returnBtn1)  //回主介面的按鈕
-        val intent2 = Intent(this, MainActivity::class.java)  //回主介面用的intent
-
+        //------TextView相關-------------------------------------------------------------------------
         //(接收前一個頁面傳來的資料) 顯示儲存的最後一次辨識的時間
         val testTextView = findViewById<TextView>(R.id.test).apply {
             text = intent.getStringExtra("LastTime")  //從主介面傳送來的時間資料
         }
-
-        val registerBtn =  findViewById<Button>(R.id.button_appUser)  //註冊帳號用的button
-        loginBtn =  findViewById(R.id.button_login)  //登入用的button
-
-        val joinBtn =  findViewById<Button>(R.id.button_joinTheRoom)  //加入房間用的button
-        val reg_log_status = findViewById<TextView>(R.id.reg_log_status)  //顯示登入或註冊的狀態
-
         val userInfo = findViewById<TextView>(R.id.userInfo)
-        if(loggedInUser.getName() != ""){  //如果有任何人登入, 則顯示目前使用者的名字
-            userInfo.text = "Welcome back, " + loggedInUser.getName()
-            loginBtn.text = "Logout"
-        }
+        //-----------------------------------------------------------------------------------------
 
-        //設定按下按鈕後會觸發的動作
+        //------回主介面相關--------------------------------------------------------------------------
+        val returnBtn = findViewById<Button>(R.id.returnBtn1)  //回主介面的按鈕
+        val intentBackToMain = Intent(this, MainActivity::class.java)  //回主介面用的intent
+
         returnBtn.setOnClickListener{
-            intent2.putExtra("LastTime_fromSec", testTextView.text)  //設定傳送回去的資料
-            intent2.putExtra("CurrentUser", loggedInUser.getName())
-            setResult(RESULT_OK,intent2)  //回傳表示執行成功的結果
+            intentBackToMain.putExtra("LastTime_fromSec", testTextView.text)  //設定傳送回去的資料
+            intentBackToMain.putExtra("CurrentUser", loggedInUser.getName())
+            setResult(RESULT_OK,intentBackToMain)  //回傳表示執行成功的結果
             finish()
         }
+        //-----------------------------------------------------------------------------------------
+
+        //------註冊相關-----------------------------------------------------------------------------
+        val registerBtn =  findViewById<Button>(R.id.button_appUser)  //註冊帳號用的button
 
         registerBtn.setOnClickListener {
             val intentToRegister = Intent(this, AppUserRegister::class.java)
             startActivityForResult(intentToRegister, 112401002)
         }
+        //-----------------------------------------------------------------------------------------
 
+        //------登入身分相關--------------------------------------------------------------------------
+        loginBtn =  findViewById(R.id.button_login)  //登入用的button
         loginBtn.setOnClickListener {
             if(loggedInUser.getName() != "" && loggedInUser.checkStatus()){
                 AlertDialog.Builder(loginBtn.context)
@@ -66,6 +64,32 @@ class SecActivity : AppCompatActivity() {
                 startActivityForResult(intentToLogin, 112401003)
             }
         }
+        //-----------------------------------------------------------------------------------------
+
+        //------加入房間相關--------------------------------------------------------------------------
+        val joinBtn =  findViewById<Button>(R.id.button_joinTheRoom)  //加入房間用的button
+
+        joinBtn.setOnClickListener {
+            if(!loggedInUser.checkStatus()){  //如果沒登入則出現提示訊息
+                AlertDialog.Builder(joinBtn.context)
+                    .setTitle("尚未登入")
+                    .setMessage("請先登入帳號才能加入或查看房間!")
+                    .setNeutralButton("ok"){dialog, which ->}
+                    .show()
+            }
+            else{
+                val intentToRoom = Intent(this, RoomStatus::class.java)
+                startActivityForResult(intentToRoom, 112401004)
+            }
+        }
+        //-----------------------------------------------------------------------------------------
+
+        //------還是TextView相關, 只是這段如果寫在太前面會發生莫名bug導致App當掉,這邊不要動-------------------
+        if(loggedInUser.getName() != ""){  //如果有任何人登入, 則顯示目前使用者的名字
+            userInfo.text = "Welcome back, " + loggedInUser.getName()
+            loginBtn.text = "Logout"
+        }
+        //-----------------------------------------------------------------------------------------
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -75,17 +99,17 @@ class SecActivity : AppCompatActivity() {
         //--------判定回傳的request code--------
         if(requestCode == 112401002){
             //requestCode傳回來時,顯示user資訊的物件根據resultCode的不同來顯示不同資訊
-            if(resultCode == RESULT_OK){
+            if(resultCode == RESULT_OK){  //002:註冊
                 reg_log_status.text = "Register succeeded"
             }
             else if(resultCode == RESULT_CANCELED){
                 reg_log_status.text = "Register canceled"
             }
             else{
-                reg_log_status.text = "ERROR"
+                reg_log_status.text = "REGISTER ERROR"
             }
         }
-        else if(requestCode == 112401003){
+        else if(requestCode == 112401003){  //003:登入
             if(resultCode == RESULT_OK){
                 userInfo.text = "Welcome back, " + loggedInUser.getName()
                 loginBtn.text = "Logout"
@@ -96,7 +120,18 @@ class SecActivity : AppCompatActivity() {
                 }
             }
             else{
-                reg_log_status.text = "ERROR"
+                reg_log_status.text = "LOGIN ERROR"
+            }
+        }
+        else if(requestCode == 112401004){  //004:房間相關資訊
+            if(resultCode == RESULT_OK){
+
+            }
+            else if(resultCode == RESULT_CANCELED){
+
+            }
+            else{
+                reg_log_status.text == "ROOM ERROR"
             }
         }
     }
