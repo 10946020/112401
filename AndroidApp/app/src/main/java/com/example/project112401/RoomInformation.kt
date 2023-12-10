dbae9429e7e3e700f32997034ca3e111801ebbeb
10946020 <10946020@ntub.edu.tw>
2023-12-10 15:44:23 +0800

    App Update : 新增了房間資訊的4個Fragment頁面, 用底下按鈕可以選擇切換
    接下來要把房間資訊導入相對應的fragment並顯示

--- AndroidApp/app/src/main/java/com/example/project112401/RoomInformation.kt
+++ AndroidApp/app/src/main/java/com/example/project112401/RoomInformation.kt
@@ -1,18 +1,44 @@
 package com.example.project112401
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class RoomInformation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room_information)
 
+        val navigator = findViewById<BottomNavigationView>(R.id.roomInfo_navigator)
+
         val backBtn = findViewById<Button>(R.id.roomInfo_backBtn)
 
-        backBtn.setOnClickListener {
+        backBtn.setOnClickListener {  //右上角返回按鈕
             finish()
         }
+        replaceFragment(RoomFragment_Info())
+
+        navigator.setOnItemSelectedListener {
+            when(it.itemId){
+
+                R.id.roomNavBtn_info -> replaceFragment(RoomFragment_Info())
+                R.id.roomNavBtn_userList -> replaceFragment(RoomFragment_UserList())
+                R.id.roomNavBtn_ioRecords -> replaceFragment(RoomFragment_ioRecord())
+                R.id.roomNavBtn_setting -> replaceFragment(RoomFragment_Setting())
+
+                else -> { true }
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment): Boolean {  //更換fragment的function
+        val fragmentManager = supportFragmentManager  //管理及監聽fragment
+        val fragmentTransaction = fragmentManager.beginTransaction()  //可對fragment進行載入, 顯示, 隱藏等操作
+        fragmentTransaction.replace(R.id.roomInfo_frameLayout, fragment)  //把frameLayout裡面換成指定的fragment
+        fragmentTransaction.commit()
+        return true
     }
 }
